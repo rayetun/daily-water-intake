f4a3fdf8e81d517c4a82feccc7f0e73d31e9d274
Md Rayhan Uddin <rayetun@gmail.com>
2021-05-31 22:41:43 +0600

    Final Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigGlass()
 
 smallGlasses.forEach((glass, index) => {
 	glass.addEventListener('click',() => highlightGlass(index))
@@ -19,4 +20,26 @@ function highlightGlass(index) {
 			glass.classList.remove('full')
 		}
 	})
+	updateBigGlass()
+}
+
+function updateBigGlass(){
+	const fullGlasses = document.querySelectorAll('.small-glass.full').length
+	const totalGlasses = smallGlasses.length
+
+	if(fullGlasses === 0){
+		percentage.style.visibility = 'hidden'
+		percentage.style.height = 0
+	}else{
+		percentage.style.visibility = 'visible'
+		percentage.style.height = `${330/totalGlasses*fullGlasses}px`
+		percentage.innerText = `${fullGlasses / totalGlasses * 100}%`
+	}
+	if(fullGlasses === totalGlasses){
+		remained.style.visibility = 'hidden'
+		remained.style.height = 0;
+	}else{
+		remained.style.visibility = 'visible'
+		liters.innerText = `${4 - (500 * fullGlasses / 1000)} Liters`
+	}
 }
